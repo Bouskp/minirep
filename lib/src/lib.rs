9f0f7a4fed0650195fa0e6db53833da5f0c6854e
bouskp <kouakouderek132@gmail.com>
2023-11-17 17:01:28 +0000

    ajout des tests

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,8 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+
 pub struct Config {
     pub recherche : String,
     pub fichier : String,
@@ -8,37 +13,84 @@ impl Config {
         if args.len() < 3  {
             return Err("il n'y a pas assez de paramètres.");
         }
-        let recherche = args[1];
-        let fichier = args[2];
+        let recherche = &args[1];
+        let fichier = &args[2];
 
-        Config {
-            recherche,
-            fichier,
-        }
+        Ok(Config {
+            recherche : recherche.to_string(),
+            fichier : fichier.to_string(),
+        })
     }
 
-    pub fn get_recherche(config : &Config) -> String {
-        config.recherche
+    pub fn get_recherche(&self) -> String {
+        self.recherche.to_string()
     }
 
-    pub fn get_fichier(config : &Config) -> String {
-        config.fichier
+    pub fn get_fichier(&self) -> String {
+        self.fichier.to_string()
+    }
+
+    pub fn set_recherche(&mut self, value : &String) {
+        self.recherche = value.to_string();
+    }
+
+    pub fn set_fichier(&mut self, value: &String) {
+        self.fichier = value.to_string();
     }
 }
 
 
 // Ouverture de fichier
+pub fn ouverture(config : &Config)-> Result<File, io::Error> {
+    match File::open(config.get_fichier()) {
+        Err(e) => Err(e),
+        Ok(f) => Ok(f),
+    }
+}
 
 // Lecture du fichier
+pub fn lecture(file: &mut File) -> BufReader<&mut File> {
+     let reader = BufReader::new(file);
+     return reader;
+}
 
+
+pub fn recherche(config: &Config, contenu : BufReader<&mut File>) -> HashMap<usize, String> {
+    let mut result = HashMap::new();
+
+    for (line, value) in contenu.lines().enumerate() {
+        let value = value.ok().unwrap();
+        if value.contains(&config.recherche) {
+            result.insert(line, value);
+        };
+    }
+
+    return result;
+}
 
 #[cfg(test)]
 mod tests {
+    use std::fs::{self, metadata};
     use super::*;
 
+    fn set_config() -> Config {
+        let config = Config::new(&["".to_string(), "derek".to_string(), "src/derek.txt".to_string()]).expect("une erreur est survenue");
+        config
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_ouverture() {
+        let config = set_config();
+        let result = ouverture(&config);
+        let medata = fs::metadata(config.get_fichier()).unwrap();
+        let file_type = medata.file_type();
+        assert_eq!(file_type.is_file(), true);
+    }
+
+    #[test]
+    fn test_lecture() {
+        let mut file = File::open(set_config().get_fichier()).unwrap();
+        let result = lecture(&mut file);
+        assert_eq!(result.lines().count(), 3 );
     }
 }
