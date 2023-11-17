03301875cffdaf4ec0fcfad2ad2893ca6d5974f7
bouskp <kouakouderek132@gmail.com>
2023-11-17 12:13:56 +0000

    ajout des méthodes et de Config

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,6 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Config {
+    pub recherche : String,
+    pub fichier : String,
 }
+
+impl Config {
+    pub fn new(args : &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3  {
+            return Err("il n'y a pas assez de paramètres.");
+        }
+        let recherche = args[1];
+        let fichier = args[2];
+
+        Config {
+            recherche,
+            fichier,
+        }
+    }
+
+    pub fn get_recherche(config : &Config) -> String {
+        config.recherche
+    }
+
+    pub fn get_fichier(config : &Config) -> String {
+        config.fichier
+    }
+}
+
+
+// Ouverture de fichier
+
+// Lecture du fichier
+
 
 #[cfg(test)]
 mod tests {
